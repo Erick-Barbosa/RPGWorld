53928a4e224dfc0379bcd3199a74945197fcab47
erick <erick.barbosa01@gmail.com>
2022-12-07 15:00:52 -0300

    Initial commit

---
+++ src/main/java/net/eriki/rpgworld/RPGWorld.java
@@ -0,0 +1,35 @@
+package net.eriki.rpgworld;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(RPGWorld.MOD_ID)
+public class RPGWorld
+{
+    public static final String MOD_ID = "rpgworld";
+    public RPGWorld()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {}
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {}
+    }
+}
